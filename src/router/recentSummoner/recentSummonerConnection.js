78bf41d174b1b555c8e1be70a8fe889217e65dd4
nswon <swnam0729@gmail.com>
2023-09-30 15:41:04 +0900

    fix

--- src/router/recentSummoner/recentSummonerConnection.js
+++ src/router/recentSummoner/recentSummonerConnection.js
@@ -11,8 +11,8 @@ export default (socket) => {
     const existsMe = await redisClient.exists(redisKey);
 
     if (existsMe) {
-      const foundMe = await redisClient.hGetAll(redisKey);
-      const me = JSON.parse(foundMe.summoner);
+      const foundMe = await redisClient.get(redisKey);
+      const me = JSON.parse(foundMe);
 
       const newRecentSummonerIdList = [];
       const recentSummonerList = await Promise.all(
@@ -23,8 +23,8 @@ export default (socket) => {
           if (existsRecentSummoner) {
             newRecentSummonerIdList.push(recentSummonerId);
 
-            const foundRecentSummoner = await redisClient.hGetAll(redisKey);
-            const recentSummoner = JSON.parse(foundRecentSummoner.summoner);
+            const foundRecentSummoner = await redisClient.get(redisKey);
+            const recentSummoner = JSON.parse(foundRecentSummoner);
             return recentSummoner.details;
           }
         }),
@@ -33,7 +33,7 @@ export default (socket) => {
       callback(recentSummonerList);
       me.recentSummonerIdList = newRecentSummonerIdList;
 
-      await redisClient.hSet(redisKey, JSON.stringify(me));
+      await redisClient.set(redisKey, JSON.stringify(me));
       await redisClient.expire(redisKey, EXPIRE_TIME);
 
       //
@@ -46,21 +46,19 @@ export default (socket) => {
         recentSummonerIdList,
       };
 
-      await redisClient.hSet(redisKey, {
-        summoner: JSON.stringify(me),
-      });
+      await redisClient.set(redisKey, JSON.stringify(me));
       await redisClient.expire(redisKey, EXPIRE_TIME);
     }
   });
 
   socket.on('start-in-game', async (summonerIdList) => {
     const redisKey = socket.summonerId.toString() + 'recent';
-    const foundMe = await redisClient.hGetAll(redisKey);
-    const me = JSON.parse(foundMe.summoner);
+    const foundMe = await redisClient.get(redisKey);
+    const me = JSON.parse(foundMe);
 
     me.recentSummonerIdList = me.recentSummonerIdList.concat(summonerIdList);
 
-    await redisClient.hSet(redisKey, JSON.stringify(me));
+    await redisClient.set(redisKey, JSON.stringify(me));
     await redisClient.expire(redisKey, EXPIRE_TIME);
     console.log('최근 함께한 소환사 업데이트');
   });
