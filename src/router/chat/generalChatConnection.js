b879daea29fb1421c20c786fd1128579b68bb712
nswon <swnam0729@gmail.com>
2023-10-16 15:03:40 +0900

    fix

--- src/router/chat/generalChatConnection.js
+++ src/router/chat/generalChatConnection.js
@@ -4,9 +4,11 @@ import redisClient from '../../lib/redisClient.js';
 
 dayjs.locale('ko');
 
+const key = 'main-chat';
+
 export default (io, socket) => {
   socket.on('init', async (callback) => {
-    const messageList = (await redisClient.lRange('main-chat', 0, 99)).map(JSON.parse);
+    const messageList = (await redisClient.lRange(key, 0, 99)).map(JSON.parse);
     callback(messageList);
   });
 
@@ -24,29 +26,29 @@ export default (io, socket) => {
     };
 
     io.emit('message', _data);
-
-    await redisClient.rPush('main-chat', JSON.stringify(_data));
+    await redisClient.rPush(key, JSON.stringify(_data));
   });
 
   socket.on('before-message', async (page) => {
     if (page) {
-      const pageNumber = parseInt(page.toString() + '00');
-      const length = await redisClient.lLen('main-chat');
+      const _page = parseInt(page.toString() + '00');
+      const length = await redisClient.lLen(key);
 
       let messageList;
       let isLast = false;
 
-      if (pageNumber + 100 >= length) {
-        messageList = await redisClient.lRange('main-chat', pageNumber, -1);
+      if (_page + 100 >= length) {
+        messageList = await redisClient.lRange(key, _page, -1);
         isLast = true;
       } else {
-        messageList = await redisClient.lRange('main-chat', pageNumber, pageNumber + 100);
+        messageList = await redisClient.lRange(key, _page, _page + 100);
       }
 
       const data = {
         messageList: messageList.map(JSON.parse),
         isLast,
       };
+
       socket.emit('response-before-message', data);
     }
   });
